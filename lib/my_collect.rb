f6278506ddc69abbc76d8feff3d49a8634f9db65
Mimi Ho <somymimiho@gmail.com>
2021-02-13 19:18:26 -0600

    commit tree 27bfac105c304611856661cae5e07e0f349613ca

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array) 
+    
+    i = 0
+    result = []
+  
+    while i < array.size
+      block_given? # optional
+      result << yield(array[i])  
+      i += 1
+    end
+  result
+  end
